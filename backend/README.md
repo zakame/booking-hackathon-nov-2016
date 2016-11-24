0d7611840f69e3513784a17a4eb8d353eaee203c
Ray Dino <ray.e.dino@gmail.com>
2016-11-24 16:42:53 +0800

    Added Django boilerplate code

---
+++ backend/README.md
@@ -0,0 +1,32 @@
+# Django Project Boilerplate
+
+* Requires PostgreSQL
+* Update this README when used on other projects
+
+## Setup
+
+- Install requirements 
+```
+pip install -r requirements.txt
+```
+- Set environment variables:
+```
+DB_NAME=XXXXXXXXXXXXXXX
+DB_USER=XXXXXXXXXXXXXXX
+DB_PASSWORD=XXXXXXXXXXX
+DB_HOST=XXXXXXXXXXXXXXX [default:127.0.0.1]
+DB_PORT=XXXXXXXXXXXXXXX [default:5432]
+DEBUG=True|False
+```
+- Sync database
+```
+python manage.py syncdb
+```
+- Run migrations (if any)
+```
+python manage.py migrate
+```
+- Create superuser 
+```
+python manage.py createsuperuser
+```
---
+++ backend/base/settings.py
@@ -0,0 +1,121 @@
+"""
+Django settings for base project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.9/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.9/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'vps97u(n^(gqmhab7m$mirjd1q2s0$eke(neny4xvzp$-82%#n'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get('DEBUG', False)
+
+ALLOWED_HOSTS = ['*']
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+)
+
+ROOT_URLCONF = 'base.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'base.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
+
+DB_NAME = os.environ.get('DB_NAME')
+DB_USER = os.environ.get('DB_USER')
+DB_PASSWORD = os.environ.get('DB_PASSWORD')
+DB_HOST = os.environ.get('DB_HOST', '127.0.0.1')
+DB_PORT = os.environ.get('DB_PORT', '5432')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': DB_NAME,
+        'USER': DB_USER,
+        'HOST': DB_HOST,
+        'PORT': DB_PORT
+    }
+}
+
+if DB_PASSWORD:
+    DATABASES['default']['PASSWORD'] = DB_PASSWORD
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+
+# URL for static files
+STATIC_URL = '/static/'
+
+# Source/s of static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
+
+# Destination of static files after collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
