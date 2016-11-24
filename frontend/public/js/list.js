dbb492b70e0caff950f37cbf887d07afecb657b4
Darryl Ong <darryl.ong.external@g4s.com>
2016-11-24 22:54:17 +0800

    Added dynamic rendering of conference list returned by a service, using underscore for templating

--- frontend/public/js/list.js
+++ frontend/public/js/list.js
@@ -1,3 +1,23 @@
+$(document).ready(function() {
+  renderConferenceList(queryConferences());
+});
+
 var goToDetails = function(confId) {
   window.location.href = "/details.html?confId=" + confId;
+}
+
+function renderConferenceList(conferences) {
+  console.log("RENDERING...");
+  //retrieve list and render
+  _.templateSettings.variable = "rc";
+  // Grab the HTML out of our template tag and pre-compile it.
+  var template = _.template(
+    $( "script.template" ).html()
+  );
+
+  var templateData = {};
+  templateData.listItems = conferences;
+
+  $("#conferenceList").empty();
+  $("#conferenceList").html(template(templateData));
 }
--- frontend/public/js/services.js
+++ frontend/public/js/services.js
@@ -1,4 +1,4 @@
-function queryConferences = function(queryString) {
+var queryConferences = function(queryString) {
   //mocked data
   return [
     {
