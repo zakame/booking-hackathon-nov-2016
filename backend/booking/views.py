22ae93ad8f71899e7e4505bf56e4b875c2ed0416
Ray Dino <ray.e.dino@gmail.com>
2016-11-25 00:01:08 +0800

    - filtered conferences list to future events - ordered by date ascending

--- backend/booking/views.py
+++ backend/booking/views.py
@@ -1,10 +1,11 @@
 from rest_framework import generics
+from datetime import datetime
 from .models import Conference
 from .serializers import ConferenceSerializer
 
 
 class ConferenceListView(generics.ListAPIView):
-    queryset = Conference.objects.all()
+    queryset = Conference.objects.filter(start_date__gte=datetime.now()).order_by('start_date')
     serializer_class = ConferenceSerializer
 
 
