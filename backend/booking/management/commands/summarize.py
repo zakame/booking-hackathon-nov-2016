00fe8f8a8f64c8497fa6c7a730545b49b5da73a1
Ray Dino <ray.e.dino@gmail.com>
2016-11-25 19:58:56 +0800

    new summarizer api key

--- backend/booking/management/commands/summarize.py
+++ backend/booking/management/commands/summarize.py
@@ -10,7 +10,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **kwargs):
         for conf in Conference.objects.filter(start_date__gte=datetime.now(), summary__isnull=True).iterator():
-            summ = requests.post('http://api.smmry.com/&SM_API_KEY=F60EC4301B&SM_LENGTH=5&SM_URL={}'.format(conf.url))
+            summ = requests.post('http://api.smmry.com/&SM_API_KEY=342335E759&SM_LENGTH=5&SM_URL={}'.format(conf.url))
             if summ.status_code == 200:
                 try:
                     conf.summary = summ.json()['sm_api_content']
